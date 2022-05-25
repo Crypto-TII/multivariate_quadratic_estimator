22bc3d3df86efd0115083ae882d7e503ff05d904
Javier Verbel <javier.verbel@tii.ae>
2022-05-25 05:55:11 +0000

    Merged in MCT21-150-add-memory-complexity-of-given (pull request #54)
    MCT21-150 add memory complexity of given
    
    * improved time complexity for cgmta
    
    * scrip to compute table1
    
    * bug fixed
    
    * Merge branch 'MCT21-149-bug-mq_estimator-bit-memory' into MCT21-148-improved-time-of-cgmta
    
    * update
    
    * update2
    
    * crosbred with memory with imput params
    
    * memory given params dinur2
    
    * memory given params hybrid-f5
    
    * Merge branch 'master' into MCT21-150-add-memory-complexity-of-given
    
    # Conflicts:
    #	src/mpkc/table1.py
    
    
    Approved-by: Rusydi H. Makarim

--- src/mpkc/table1.py
+++ src/mpkc/table1.py
@@ -1,4 +1,5 @@
 from mpkc import MQEstimator
+from sage.functions.log import log
 
 #Table 1
 #best algorithms column
@@ -32,3 +33,52 @@ print(E5.table(precision=1))
 print("Type VI")
 E6 = MQEstimator(n=30, m=20, q=31, w=w, theta=theta)
 print(E6.table(precision=1))
+
+
+#Algorithm used (theoretical)
+#Run the folowing lines of codes to generate the entries
+#corresponding to the column Algorithm used (theoretical)
+
+
+from mpkc import MQEstimator
+E = MQEstimator(n=74, m=148, q=2, w=2.81, theta=2, h=10)
+time = E.crossbred.time_complexity(k=22,D=4,d=1)
+memory = E.crossbred.memory_complexity(k=22,D=4,d=1)
+print("Row 1 (Type I)")
+print(float(log(time,2)), float(log(memory,2)))
+
+E = MQEstimator(n=99, m=66, q=2, w=2.81, theta=2, h=0)
+time = E.exhaustive_search.time_complexity()
+memory = E.exhaustive_search.memory_complexity()
+print("Row 2 (Type IV)")
+print(float(log(time, 2)), float(log(memory, 2)))
+
+E = MQEstimator(n=103, m=69, q=2, w=2.81, theta=2, h=17)
+time = E.crossbred.time_complexity(k=15, D=4, d=1)
+memory = E.crossbred.memory_complexity(k=15, D=4, d=1)
+print("Row 3 (Type IV)")
+print(float(log(time, 2)), float(log(memory, 2)))
+
+E = MQEstimator(n=37, m=74, q=2**8, w=2.81, theta=2, h=0)
+time = E.f5.time_complexity()
+memory = E.f5.memory_complexity()
+print("Row 4 (Type II)")
+print(float(log(time, 2)), float(log(memory, 2)))
+
+E = MQEstimator(n=28, m=19, q=2**8, w=2.81, theta=2, h=1)
+time = E.hybrid_f5.time_complexity()
+memory = E.hybrid_f5 .memory_complexity()
+print("Row 5 (Type V)")
+print(float(log(time, 2)), float(log(memory, 2)))
+
+E = MQEstimator(n=38, m=76, q=31, w=2.81, theta=2, h=0)
+time = E.boolean_solve_fxl.time_complexity()
+memory = E.boolean_solve_fxl.memory_complexity()
+print("Row 6 (Type III)")
+print(float(log(time, 2)), float(log(memory, 2)))
+
+E = MQEstimator(n=30, m=20, q=31, w=2.81, theta=2, h=2)
+time = E.hybrid_f5.time_complexity()
+memory = E.hybrid_f5.memory_complexity()
+print("Row 6 (Type III)")
+print(float(log(time, 2)), float(log(memory, 2)))
